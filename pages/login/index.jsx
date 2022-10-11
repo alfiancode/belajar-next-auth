9d66915cbadd3b141dc6e7db17a0d4143d24b0ef
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-10-11 14:23:17 +0700

    Update index.jsx

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -12,7 +12,7 @@ const login = () => {
               <div className="grid gap-6 sm:grid-cols-2">
                 <button
                   className="h-11 rounded-full border border-gray-300/75 bg-white px-6 transition active:bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-800 dark:hover:border-gray-700"
-                  onClick={() => signIn()}
+                  onClick={() => signIn("google")}
                 >
                   {/* google button */}
                   <div className="flex items-center justify-center space-x-4">
